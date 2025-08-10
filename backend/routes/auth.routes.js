f2903b508840b07a00403a32529986eebdef13a5
lisa <lisa@gmail.com>
2025-08-10 14:42:46 +0800

    34.BackendSetup

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { signup, login, logout } from '../controllers/auth.controller.js'; // Importing auth controller functions
+
+
+const router = express.Router();
+
+router.post('/signup', signup);
+
+router.post('/login', login);
+
+router.post('/logout', logout);
+
+export default router;
