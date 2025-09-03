ca1347229af85765aa014a55baf8bf713f2ed13f
lisa <lisa@gmail.com>
2025-09-03 22:59:19 +0800

    46.deplayment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,23 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.router.js'; // Importing user routes
 import cors from 'cors';
 import { app, server } from './socket/socket.js';
+import path from 'path';
+
+
+
+
+
+//for deployment
+const __dirname = path.resolve(); // To get the directory name of the current module
+
+//for deployment
+//加入前端的build靜態檔案
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+
 
 // const app = express();
 dotenv.config();//用於可以讀取設定的env檔案
@@ -28,6 +45,11 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+
+
+
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
